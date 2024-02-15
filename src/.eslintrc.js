c2a8af4efc9e0a2a85a9d932542e9785b76712ea
Dzzjio <giojio14@gmail.com>
2024-02-15 20:16:52 +0400

    fix file formats crlf to lf

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,16 +1,16 @@
-// module.exports = {
-//     parser: '@typescript-eslint/parser',
-//     extends: [
-//         'plugin:react/recommended',
-//         'plugin:@typescript-eslint/recommended',
-//         'prettier', // Update this line
-//     ],
-//     plugins: ['react', '@typescript-eslint', 'prettier'],
-//     rules: {
-//         'prettier/prettier': 'error',
-//         'react/prop-types': 'off',
-//         '@typescript-eslint/ban-ts-ignore': 'off',
-//         '@typescript-eslint/ban-ts-comment': 'off',
-//         'react/react-in-jsx-scope': 'off',
-//     },
-// }
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/prop-types': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+}
