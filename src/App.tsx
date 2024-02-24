d05dabb26c3c501444d428432c82501f77f4ce72
Dzzjio <giojio14@gmail.com>
2024-02-24 22:13:59 +0400

    save app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Footer from 'components/layout/footer/footer'
 import Header from 'components/layout/header/header'
-import HeaderBotYellow from 'components/layout/headerBotYellow/headerBotY'
 import Routes from 'routes'
 
 function App() {
