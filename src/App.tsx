aba5fd25cd3613fe3f6c7cf02f4c592ead0fa380
Dzzjio <giojio14@gmail.com>
2024-02-24 22:13:15 +0400

    remove yello header from app and put it where needed

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <div>
       <Header />
-      <HeaderBotYellow imgUrl={'https://media.wired.com/photos/5e9110887b575f0008effd4f/master/pass/Culture_ps5_49747503557_566da5ebc8_3k.jpg'} />
       <Routes />
       <Footer />
     </div>
--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -4,20 +4,24 @@ import { Container } from "components/styled/container"
 import { ShopConcainer } from "./styled"
 import { Title } from "components/styled/title"
 import ColorSidebar from "components/layout/colorSideBar/colorSideBar"
+import HeaderBotYellow from "components/layout/headerBotYellow/headerBotY"
 
 const Shop = () => {
     return (
-        <Container>
-            <ShopConcainer>
-            <Title>Shop</Title>
-            <div className="row">
-                <ColorSidebar />
-                <div className="col-8">
-                    <PopularProducts />
+        <>
+            <HeaderBotYellow imgUrl={'https://media.wired.com/photos/5e9110887b575f0008effd4f/master/pass/Culture_ps5_49747503557_566da5ebc8_3k.jpg'} />
+            <Container>
+                <ShopConcainer>
+                <Title>Shop</Title>
+                <div className="row">
+                    <ColorSidebar />
+                    <div className="col-8">
+                        <PopularProducts />
+                    </div>
                 </div>
-            </div>
-            </ShopConcainer>
-        </Container>
+                </ShopConcainer>
+            </Container>
+        </>
     )
 }
 
