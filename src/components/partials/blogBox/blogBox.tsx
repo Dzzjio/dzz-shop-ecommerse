9d04ed52884983cd697a56b501600fe66640a8aa
Dzzjio <giojio14@gmail.com>
2024-02-24 17:05:04 +0400

    add blogBox component

--- src/components/partials/blogBox/blogBox.tsx
+++ src/components/partials/blogBox/blogBox.tsx
@@ -1,9 +1,18 @@
-import { StyledBlogBox } from "./styled";
+import { StyledBlogBox, StyledReadMore } from "./styled";
 
 const BlogBox = () => {
     return (
         <StyledBlogBox>
-            test
+            <img src="https://sm.ign.com/t/ign_in/news/j/jack-black/jack-black-praises-hbos-the-last-of-us-wants-red-dead-redemp_5f2a.1280.jpg" alt="testimgps5" />
+            <div>
+                <div>
+                    <p> 17 MAY 2024</p>
+                </div>
+                <p>
+                    Uncovering The beauty benefits of red dead redemption.
+                </p>
+                <StyledReadMore to='/'>Read More &gt;</StyledReadMore>
+            </div>
         </StyledBlogBox>
     )   
 }
--- src/components/partials/blogBox/styled.ts
+++ src/components/partials/blogBox/styled.ts
@@ -1,5 +1,58 @@
 import styled from "styled-components";
+import { Link } from 'react-router-dom';
 
 export const StyledBlogBox = styled.div`
-    
+    width: 350px;
+    height: 350px;
+    border-radius: 20px;
+    overflow: hidden;
+    position: relative;
+
+    & > img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    & > div {
+        position: absolute;
+        width: 60%;
+        height: 33%;
+        bottom: 0;
+        left: 0;
+        background-color: black;
+        z-index: 10;
+        border-radius: 0 20px 0 0;
+        padding: 20px;
+
+        & > p {
+            margin: 8px 0;
+        }
+
+        & > div {
+            background-color: #fff;
+            display: inline-block;
+            border-radius: 5px;
+
+            & > p {
+                font-size: 10px;
+                font-weight: bold;
+                color: #000;
+                padding: 5px;
+                margin: 0;
+            }
+        }
+
+        & > * {
+            color: white;
+        }
+    }
+`
+
+export const StyledReadMore = styled(Link)`
+    text-decoration: none;
+    font-size: 12px;
 `
