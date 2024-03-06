06531e8145a5aeb01ffe394997f6b50fecc774fb
Dzzjio <giojio14@gmail.com>
2024-03-06 17:36:00 +0400

    add favorites page

--- src/pages/wishlist/index.tsx
+++ src/pages/wishlist/index.tsx
@@ -1,10 +1,60 @@
-import { S } from "./styled"
+import { AddCartIcon, WishListContainer, WishListContent, WishListTitle } from "./styled"
+import img from '../../assets/images/product-2.jpg'
+import { FC } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCartArrowDown, faXmark } from "@fortawesome/free-solid-svg-icons"
+import HeaderBotYellow from "components/layout/headerBotYellow/headerBotY"
 
-const WishListPage = () => {
+
+const wishlistItmes = [
+    {
+        product: 'product 1',
+        productImgUrl: img,
+        price: 333,
+        inStock: true
+    },
+    {
+        product: 'product 2',
+        productImgUrl: img,
+        price: 666,
+        inStock: false
+    },
+    {
+        product: 'product 3',
+        productImgUrl: img,
+        price: 999,
+        inStock: false
+    }
+]
+
+const WishListPage: FC = () => {
     return (
-        <S>
-            wishlist
-        </S>
+        <>
+        <HeaderBotYellow imgUrl={img} />
+        <WishListContainer>
+            <WishListTitle>
+                <p>Product</p>
+                <p>Price</p>
+                <p>Stock</p>
+            </WishListTitle>
+
+            <WishListContent>
+                {wishlistItmes.map((item, index) => (
+                    <div key={index}>
+                        <div>
+                            <div><FontAwesomeIcon icon={faXmark}/></div>
+                            <img src={item.productImgUrl} alt={item.product} />
+                            <h3>{item.product}</h3>
+                        </div>
+                        <p>Price: ${item.price}</p>
+                        <p>{item.inStock ? "In Stock" : "Out of Stock"}</p>
+                        <button>Add To Cart</button>
+                        <AddCartIcon><FontAwesomeIcon icon={faCartArrowDown}/></AddCartIcon>
+                    </div>
+                ))}
+            </WishListContent>
+        </WishListContainer>
+        </>
     )
 }
 
--- src/pages/wishlist/styled.ts
+++ src/pages/wishlist/styled.ts
@@ -1,5 +1,118 @@
 import styled from "styled-components";
 
-export const S = styled.div`
-    
+export const WishListContainer = styled.div`
+    width: 60vw;
+    margin: 60px auto;
+    display: flex;
+    flex-direction: column;
+`
+
+export const WishListTitle = styled.div`
+    width: 100%;
+
+    display: flex;
+    justify-content: space-around;
+    border-bottom: 1px solid gray;
+
+    & > p:last-child {
+        width: 11vw;
+    }
+`
+
+export const WishListContent = styled.div`
+    width: 100%;
+
+    display: flex;
+    flex-direction: column;
+
+    & > div {
+        display: flex;
+        justify-content: space-between;
+        border-bottom: 1px solid gray;
+        align-items: center;
+        padding: 15px 0;
+
+        & > p {
+            @media (max-width: 768px) {
+                font-size: 13px;
+            }
+        }
+
+        & > p:nth-child(2) {
+            width: 20%;
+            text-align: end;
+        }
+
+        & > p:nth-child(3) {
+            width: 20%;
+            text-align: center;
+        }
+
+        & > div {
+            display: flex;
+            align-items: center;
+            gap: 20px;
+
+            @media (max-width: 768px) {
+                gap: 10px;
+            }
+
+            & > div {
+                width: 25px;
+                height: 25px;
+                background-color: black;
+                color: white;
+                text-align: center;
+                line-height: 1.65;
+                border-radius: 15px;
+
+                &: hover {
+                    background-color: white;
+                    color: black;
+                }
+            }
+
+            & > img {
+                width: 50px;
+                height: 50px;
+                border-radius: 10px;
+
+                @media (max-width: 768px) {
+                    width: 40px;
+                    height: 40px;
+                }
+            }
+
+            & > h3 {
+                @media (max-width: 768px) {
+                    font-size: 14px;
+                }
+            }
+        }
+    }
+
+    button {
+        background-color: black;
+        color: white;
+        padding: 5px 10px;
+        border: 1px solid black;
+        border-radius: 5px;
+        
+        &: hover {
+            background-color: white;
+            color: black;
+        }
+
+        @media (max-width: 1024px) {
+            display: none;
+        }
+    }
+`
+
+export const AddCartIcon = styled.span`
+    display: none;
+
+    @media (max-width: 1024px) {
+        display: block;
+    }
 `
