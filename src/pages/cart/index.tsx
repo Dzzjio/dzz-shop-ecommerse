537120050bbfb2a1fc6760dcc3b81b1fb746e33c
Dzzjio <giojio14@gmail.com>
2024-03-12 16:47:16 +0400

    add cart page right section

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,10 +1,53 @@
-import { CartPageContainer } from "./styled"
+import HeaderBotYellow from "components/layout/headerBotYellow/headerBotY"
+import { CartPageContainer, CartPageLeft, CartPageRight, CartPageRightFooter, CartPageRightHeader } from "./styled"
+import airplane from '../../assets/images/air-plane.png'
+import boxProduct from '../../assets/images/box-product-icon.png'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCircleCheck } from "@fortawesome/free-regular-svg-icons"
 
 const CartPage = () => {
     return (
+        <>
+        <HeaderBotYellow imgUrl={"https://freerangestock.com/sample/61264/online-shopping--laptop-to-shopping-cart.jpg"} />
         <CartPageContainer>
-            cart page
+            <CartPageLeft>
+                ;eft
+            </CartPageLeft>
+
+            <CartPageRight>
+                <CartPageRightHeader>
+                    <div>
+                        <p>Bank Offer 5% Cashback</p>
+                    </div>
+                    <div>
+                        <img src={airplane} alt="airplane-cart" />
+                        <div>
+                            <p>Free</p>
+                            <h4>Shipping</h4>
+                        </div>
+                    </div>
+                    <div>
+                        <img src={boxProduct} alt="boxProduct" />
+                        <div>
+                            <h4>Enjoy The Product</h4>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting</p>
+                        </div>
+                    </div>
+                </CartPageRightHeader>
+                <CartPageRightFooter>
+                    <div>
+                        <FontAwesomeIcon icon={faCircleCheck} />
+                        <span>You will save ₹504 on this order</span>
+                    </div>
+                    <div>
+                        <p>Total Amount:</p>
+                        <h4>$125</h4>
+                    </div>
+                    <button>Place Order</button>
+                </CartPageRightFooter>
+            </CartPageRight>
         </CartPageContainer>
+        </>
     )
 }
 
--- src/pages/cart/styled.ts
+++ src/pages/cart/styled.ts
@@ -1,5 +1,99 @@
+import { Breakpoints } from "constantss";
 import styled from "styled-components";
 
 export const CartPageContainer = styled.section`
-    
+    width: 90vw;
+    margin: 50px auto;
+    display: flex;
+    justify-content: space-between;
+
+    @media (max-width: ${Breakpoints.tablet}) {
+        flex-direction: column;
+    } 
+`
+
+export const CartPageLeft = styled.div`
+    width: 66%;
+    background-color: red;
+
+
+
+
+    @media (max-width: ${Breakpoints.tablet}) {
+        order: 12;
+        width: 90%;
+        margin: 0 auto;
+    }
+`
+
+export const CartPageRight = styled.div`
+        width: 33%;
+        border: 1px solid black;
+        border-radius: 10px;
+        padding: 20px;
+        height: max-content;
+    }
+
+    @media (max-width: ${Breakpoints.tablet}) {
+        order: 1;
+        width: 90%;
+        margin: 50px auto;
+    } 
+`
+
+export const CartPageRightHeader = styled.div`
+    & > div {
+        border: 1px solid black;
+        border-radius: 10px;
+        margin: 10px 0;
+        padding: 10px;
+        display: flex;
+        gap: 10px;
+
+        & > * {
+            margin: 0;
+        }
+
+        & > div > * {
+            margin: 0;
+        }
+
+        & > img {
+            object-fit: cover;
+        }
+`
+
+export const CartPageRightFooter = styled.div`
+    margin-top: 20px;
+
+    & > div:first-child {
+        display: flex;
+        gap: 10px;
+    }
+
+    & > div:nth-child(2) {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        & > h4 {
+            font-size: 20px;
+        }
+    }
+
+    button {
+        background-color: black;
+        width: 100%;
+        padding: 10px;
+        border-radius: 10px;
+        border: 1px solid black;
+        color: white;
+        font-weight: bold;
+        cursor: pointer;
+
+        &:hover {
+            color: black;
+            background-color: white;
+        }
+    }
 `
