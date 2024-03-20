372066fa91d22ec905b063943488e9de76ab163d
Dzzjio <giojio14@gmail.com>
2024-03-20 14:42:47 +0400

    add home page first section

--- src/pages/home/firstSection.tsx
+++ src/pages/home/firstSection.tsx
@@ -2,6 +2,7 @@ import { StyledFirstSectionHome } from "./style"
 import redHotLogo from '../../assets/images/red-hot-logo.png'
 import yelloStar from '../../assets/images/home-page-yellow-star.png'
 import collectionsLogo from '../../assets/images/explore-more-collection.png'
+import productHome from '../../assets/images/product-1.jpg'
 
 const FirstSectionHome = () => {
     return (
@@ -38,7 +39,7 @@ const FirstSectionHome = () => {
             </div>
 
             <div className="home-first-section-right">
-                img
+                <img src={productHome} alt="productHome" />
             </div>
         </StyledFirstSectionHome>
     )
