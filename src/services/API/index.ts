905bc3068e9ba1d98fa6adbea33a517e4633362a
Dzzjio <giojio14@gmail.com>
2024-03-22 15:31:46 +0400

    add interceptor

---
+++ src/services/API/index.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { API_URL } from 'constantss';
+
+const api = axios.create({
+  baseURL: API_URL,
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
